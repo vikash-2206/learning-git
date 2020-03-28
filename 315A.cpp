12b75a357fb3354010f3b4eae93eb007667c1f52
vikash-2206 <201851144@iiitvadodara.ac.in>
2020-03-28 18:23:00 +0530

    1st code

---
+++ 315A.cpp
@@ -0,0 +1,37 @@
+//code by vikash
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	
+
+	int n;
+	cin>>n;
+	
+	int a[n],b[n],ans[1001];
+	memset(ans,0,sizeof(ans));
+	for (int i = 0; i < n; ++i)
+	{
+		/* code */
+		cin>>a[i];
+		cin>>b[i];
+		if(a[i]!=b[i])
+		{
+			ans[a[i]]++;
+			ans[b[i]]++;
+		}
+	}
+	int count=0;
+	for (int i = 0; i < 1001; ++i)
+	{
+		if (ans[i]==2)
+		{
+			count++;			/* code */
+		}
+		/* code */
+	}
+	cout<<n-count<<endl;
+		
+	 
+
+	return 0;
+}
